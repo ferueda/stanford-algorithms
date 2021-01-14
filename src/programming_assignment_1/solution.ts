1eced54253949d27fa8f27bcc3caf6a8d094bed2
ferueda <fe.rueda@outlook.com>
2021-01-13 21:39:24 -0300

    Update folder structure

--- src/programming_assignment_1/solution.ts
+++
@@ -1,8 +0,0 @@
-import karatsubaMultiplication from '../algorithms/karatsubaMultiplication';
-
-const int1 = '3141592653589793238462643383279502884197169399375105820974944592';
-const int2 = '2718281828459045235360287471352662497757247093699959574966967627';
-
-const result = karatsubaMultiplication(int1, int2);
-
-console.log(result);
