290598e1a9eaa866776cf4a996743f7c841b8949
ferueda <fe.rueda@outlook.com>
2021-01-17 21:00:13 -0300

    Add removeMin to heap

--- src/data_structures/heap.ts
+++ src/data_structures/heap.ts
@@ -5,7 +5,13 @@ class Heap {
     this.data = [];
   }
 
-  _heapify(): void {
+  getMin(): number {
+    return this.data[0];
+  }
+
+  insert(element: number): void {
+    this.data.push(element);
+
     let currentIndex = this.data.length - 1;
     let parentIndex = Math.floor((currentIndex - 1) / 2);
 
@@ -23,13 +29,38 @@ class Heap {
     }
   }
 
-  getMin(): number {
-    return this.data[0];
-  }
+  removeMin(): void {
+    if (this.data.length < 2) {
+      this.data.pop();
+      return;
+    }
 
-  insert(element: number): void {
-    this.data.push(element);
-    this._heapify();
+    this.data[0] = this.data.pop() as number;
+    let currentIndex = 0;
+    const len = this.data.length;
+
+    while (currentIndex < len) {
+      let leftNodeIndex = 2 * currentIndex + 1;
+      let rightNodeIndex = 2 * currentIndex + 2;
+
+      if (leftNodeIndex < len && this.data[leftNodeIndex] < this.data[currentIndex]) {
+        [this.data[currentIndex], this.data[leftNodeIndex]] = [
+          this.data[leftNodeIndex],
+          this.data[currentIndex],
+        ];
+        currentIndex = leftNodeIndex;
+        continue;
+      } else if (rightNodeIndex < len && this.data[rightNodeIndex] < this.data[currentIndex]) {
+        [this.data[currentIndex], this.data[rightNodeIndex]] = [
+          this.data[rightNodeIndex],
+          this.data[currentIndex],
+        ];
+        currentIndex = rightNodeIndex;
+        continue;
+      }
+
+      break;
+    }
   }
 }
 
